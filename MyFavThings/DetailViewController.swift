e46f4ec41513a8f8f4a5842d16b30bf2652fad46
akkawhistler <42915996+akkawhistler@users.noreply.github.com>
2018-09-17 18:38:14 -0500

    cac

---
+++ MyFavThings/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  MyFavThings
+//
+//  Created by cis290 on 9/17/18.
+//  Copyright © 2018 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+   
+
+
+    @IBOutlet weak var productImageView: UIImageView!
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
